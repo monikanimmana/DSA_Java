0c5c077a292490d94d19e6d4045c9320b1bfdc1f
monikanimmana <monikanimmana33@gmail.com>
2026-03-11 22:14:47 +0530

    Adding LC_121 code

---
+++ LeetCodes/Arrays/LC_121.java
@@ -0,0 +1,36 @@
+package LeetCode.LeetCodes.Arrays;
+
+public class LC_121 {
+    public void profit_loss(){
+        int arr[] = new int[]{7,3,2,1,5};
+        int n= arr.length;
+         int final_p = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]<arr[j]){
+                    int buy = arr[i];
+                    int sell = arr[j];
+                    int profit = sell - buy;
+                    if(profit > final_p){
+                        final_p = profit;
+
+                    }
+
+
+                }
+            }
+        }
+        System.out.println(final_p);
+        
+
+        }
+            
+        
+        public static void main(String[] args) {
+            LC_121 obj = new LC_121();
+            obj.profit_loss();
+            
+        }
+
+    }
+
