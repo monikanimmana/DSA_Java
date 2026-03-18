adcdfb46bbf56d59c25a058b52e58af9b737474a
monikanimmana <monikanimmana3366@gmail.com>
2026-03-18 13:58:16 +0530

    adding problem number 3

---
+++ LeetCode/Arrays/LC_3.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class LC_3 {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int i=0;
+        int max_length = 0 ;
+        int n = s.length();
+        for(int j=0;j<n;j++){
+            while(!set.add(s.charAt(j))){
+                set.remove(s.charAt(i));
+                i++;
+            }
+            max_length = Math.max(max_length,j-i+1);
+        }
+        return max_length;
+    }
+    public static void main(String[] args) {
+        LC_3 obj = new LC_3();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(obj.lengthOfLongestSubstring(s));
+    }
+    
+}
