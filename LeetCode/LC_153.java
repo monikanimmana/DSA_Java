89ac01eb8a0e1dee78cc216bc35a89799fd172b6
monikanimmana <monikanimmana3366@gmail.com>
2026-04-06 17:45:31 +0530

    Adding 153 leetcode sum

---
+++ LeetCode/LC_153.java
@@ -0,0 +1,28 @@
+public class LC_153{
+    public int minvalue(int nums[]){
+        int left=0;
+        int right=nums.length-1;
+        int min_val=Integer.MAX_VALUE;
+        while(left<=right){
+            if(nums[left]<nums[right]){
+                min_val=Math.min(min_val,nums[left]);
+            }
+            
+            int mid=(left+right)/2;
+            min_val=Math.min(min_val,nums[mid]);
+            if(nums[mid]>nums[right]){
+                left=mid+1;
+
+            }else{
+                right=mid-1;
+            }
+        }
+
+        return min_val;
+    }
+    public static void main(String[] args) {
+        LC_153 obj=new LC_153();
+        int nums[] = new int[]{11,12,13,14};
+        System.out.println(obj.minvalue(nums));
+    }
+}
