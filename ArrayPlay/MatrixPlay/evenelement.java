a2cc0a78fe361aba1454068e4fb6eb200553602d
monikanimmana <monikanimmana3366@gmail.com>
2026-04-16 20:31:30 +0530

    martix even element

--- ArrayPlay/MatrixPlay/evenelement.java
+++ ArrayPlay/MatrixPlay/evenelement.java
@@ -1,15 +1,18 @@
-public class countelement{
+public class evenelement{
     public int elementcount(int nums[][]){
         int count=0;
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums[i].length;j++){
-                count++;
+                if(nums[i][j]%2==0){
+                    count++;
+                }
+                
             }
         }
         return count;
     }
     public static void main(String[] args) {
-       countelement obj=new countelement();
+       evenelement obj=new evenelement();
        int nums[][]=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
        System.out.println(obj.elementcount(nums));
     }
