f789e396f7931cd1ec46c42413e1a067e370cff9
monikanimmana <monikanimmana3366@gmail.com>
2026-04-17 15:52:09 +0530

    Leetcode number 74

--- LeetCode/LC_74.java
+++ LeetCode/LC_74.java
@@ -1,19 +1,50 @@
 public class LC_74{
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public void searchMatrix(int[][] matrix, int target) {
+        int m=matrix[0].length;
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(target==matrix[i][j]){
-                    return true;
-                }
-                
+            if(matrix[i][0] <= target && target<=matrix[i][m-1]){
+                System.out.println(findtarget(matrix[i] , target));
+            }
+        }
+    }
+    public boolean findtarget(int matrix[] , int taregt){
+        int low=0;
+        int high=matrix.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[mid]==taregt){
+                return true;
+            }else if(matrix[mid]>taregt){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return false;
-    }
+    } 
     public static void main(String[] args) {
         LC_74 obj=new LC_74();
         int nums[][]=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
         int target=6;
-        System.out.println(obj.searchMatrix(nums, target));
+        obj.searchMatrix(nums, target);
     }
 }
+
+//optimal solution
+// int low=0;int high=m*n-1;
+// int r=nums.length;
+// int c=nums[0].length;
+// while(low<=high){
+//     int mid=low+high/2;
+//     int row=mid/c;
+//     int col=mid%c;
+//     if(nums[row][col]==target){
+//         return true;
+//     }else if(nums[row][col]>target){
+//         high=mid-1;
+//     }else{
+//         low=mid+1;
+//     }
+// }
+// return false;
+
