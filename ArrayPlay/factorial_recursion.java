5e94f8f512525ff053746838f35631e40d4140b9
monikanimmana <monikanimmana3366@gmail.com>
2026-04-23 10:37:56 +0530

    reverse array using recursion

--- ArrayPlay/factorial_recursion.java
+++ ArrayPlay/factorial_recursion.java
@@ -1,7 +1,7 @@
 public class factorial_recursion {
     public int factorial(int n){
-        if (n==1){
-            return n;
+        if (n==0 || n==1){
+            return 1;
         }
         return n * factorial(n-1);
     }
