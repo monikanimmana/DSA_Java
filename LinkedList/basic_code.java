f518644131d4501e109f81622ba918a76d25809f
monikanimmana <monikanimmana3366@gmail.com>
2026-05-16 10:04:57 +0530

    basic creation

--- LinkedList/basic_code.java
+++ LinkedList/basic_code.java
@@ -9,7 +9,22 @@ public class basic_code{
     }
     public static void main(String[] args) {
         Node n1=new Node(10);
-        System.out.println(n1.data);
+        Node n2=new Node(20);
+        Node n3=new Node(30);
+        Node n4=new Node(40);
+
+        n1.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+        
+        Node temp=n1;
+
+        while(temp!=null){
+            System.out.print(temp.data + " --> ");
+            temp=temp.next;
+        }
+
+        System.out.println("null");
 
     }
 }
