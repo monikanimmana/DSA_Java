5081b349a7a1350e36f15b6566c3691f6828df6c
monikanimmana <monikanimmana3366@gmail.com>
2026-06-15 21:14:15 +0530

    leetcode 20

---
+++ LeetCode/LC_20.java
@@ -0,0 +1,30 @@
+public class LC_20{
+    public boolean parenthese(String S){
+        int top=-1;
+        int n=S.length();
+        char stack[]=new char[n];
+        for(int i=0;i<n;i++){
+            char ch=S.charAt(i);
+            if(ch=='{' || ch=='[' || ch=='('){
+                top++;
+                stack[top]=ch;
+            }else{
+                if(top==-1) return false;
+
+                if(ch=='}'  && stack[top]!='{' || ch==']'  && stack[top]!='[' || ch==')'  && stack[top]!='('){
+                        return false;
+                }
+                top--;
+                
+            }    
+        }
+        if(top==-1) return true;
+        return false;
+
+    }
+    public static void main(String[] args) {
+        String S="}";
+        LC_20 obj=new LC_20();
+        System.out.println(obj.parenthese(S));
+    }
+}
