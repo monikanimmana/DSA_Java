3c6065eed20dd029337b0eb9ea033df1ff75aecb
monikanimmana <monikanimmana3366@gmail.com>
2026-07-07 11:07:49 +0530

    leetcode 739

---
+++ LeetCode/LC_739.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class LC_739{
+    public int[] temperature(int temp[]){
+        int n=temp.length;
+        int days[]=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.isEmpty() && temp[st.peek()] <= temp[i]){
+                st.pop();
+                
+            }
+
+            if(st.isEmpty()){
+                days[i]=0;
+            }else{
+                days[i] = st.peek()-i;
+            }
+
+            st.push(i);
+
+        }
+        return days;
+    }
+    public static void main(String[] args) {
+        LC_739 obj=new LC_739();
+        int temp[]=new int[]{73,74,76,72,72,69,74};
+        System.out.println(Arrays.toString(obj.temperature(temp)));
+    }
+}
